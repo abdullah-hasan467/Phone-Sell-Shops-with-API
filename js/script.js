cf98a864c4dc294ecd8fb83749dc11ef8e39a4a3
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2024-11-27 22:57:55 +0600

    ppp

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const displayPhones = phones => {
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.innerText='';
     phones.forEach(phone =>{
-        console.log(phone);
+        // console.log(phone);
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card bg-gray-100 w-96 shadow-xl m-10 p-10`;
         phoneCard.innerHTML = `
@@ -28,7 +28,7 @@ const displayPhones = phones => {
 <h2 class="card-title">${phone.phone_name}</h2>
 <p> ${phone.slug}</p>
 <div class="card-actions justify-end">
-<button class="btn btn-primary">Buy Now</button>
+<button onclick ="handleShowDetails('${phone.slug}')" class="btn btn-primary text-center">Show Details</button>
 </div>
 </div>
 </div>
@@ -41,10 +41,32 @@ const displayPhones = phones => {
 }
 
 
+
+
+
 const handleSearch = () => {
 const searchField = document.getElementById('search-field');
 const searchText = searchField.value;
-console.log(searchText);
+// console.log(searchText);
 loadPhone(searchText);
 }
 
+
+const handleShowDetails = async (id) =>{
+    console.log(id);
+    const res = await fetch ('https://openapi.programming-hero.com/api/phone/${id}');
+    const data = await res.json();
+    const phone = data.data
+
+   
+showPhoneDetails(phone);
+
+}
+
+const showPhoneDetails = (phone)=>{
+    show_details_modal.showModal();
+    const phoneName = document.getElementById('showDetailsPhoneName');
+    phoneName.innerText = phone.name;
+    console.log(phone)
+
+}
